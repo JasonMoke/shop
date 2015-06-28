c334ee5a6785f979669a39ebe843ad0ab8849110
高广超 <gaoguangchao@wanda.cn>
2015-06-28 22:22:28 +0800

    更改代码目录
    更改代码目录

--- mobile/data/cache/compiled/comments.lbi.php
+++
@@ -1,9 +0,0 @@
-
-<div id="ECS_COMMENT"> <?php 
-$k = array (
-  'name' => 'comments',
-  'type' => $this->_var['type'],
-  'id' => $this->_var['id'],
-);
-echo $this->_echash . $k['name'] . '|' . serialize($k) . $this->_echash;
-?></div>
